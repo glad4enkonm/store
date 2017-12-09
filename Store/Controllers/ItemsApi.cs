a1b7d30bc5f404798fc7ef143f7bc1680d67e0e9
Nikolai Gladchenko <Nikolai_Gladchenko@jabil.com>
2017-12-09 12:12:43 +0100

    First version of items api ready

--- Store/Controllers/ItemsApi.cs
+++ Store/Controllers/ItemsApi.cs
@@ -10,22 +10,51 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Store.Models;
 using Microsoft.AspNetCore.JsonPatch;
-using Microsoft.AspNetCore.Http;
+using Store.Domain.Repositories;
+using AutoMapper;
+using Store.Domain.Exceptions;
 
 namespace Store.Controllers
 {
     public class ItemsApi : Abstract.ItemsApi
-    { 
+    {
+        IItemRepository _repository;
+
+        static ItemsApi()
+        {
+            Mapper.Initialize(cfg =>
+                cfg.CreateMap<Domain.Item, Models.Item>()
+                    .ForMember(dst => dst.ItemId, opt => opt.MapFrom(src => src.Id)));
+        }
+
+        public ItemsApi(IItemRepository repository)
+        {
+            _repository = repository;
+        }
 
         public override async Task<IActionResult> GetAsync()
         {
-            throw new NotImplementedException();
+            IEnumerable<Domain.Item> list = await _repository.List();
+            return Json(Mapper.Map<Models.ItemList>(list));
         }
 
 
         public override async Task<IActionResult> GetAsync([FromRoute]long? itemId)
         {
-            throw new NotImplementedException();
+            if (!itemId.HasValue)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                Domain.Item item = await _repository.GetById(itemId.Value);
+                return Json(Mapper.Map<Models.Item>(item));
+            }
+            catch (ItemNotFoundException ex)
+            {
+                return NotFound();
+            }
         }
     }
 }
